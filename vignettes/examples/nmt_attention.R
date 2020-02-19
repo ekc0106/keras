01e59c545afcf3f011bcd995c878ba1a5fad047b
Kyucheol Eom <ekc0106@naver.com>
2020-02-19 15:32:26 +0900

    Update nmt_attention.R
    I compared your R code with my python code.
    Then I found that your indices in teacher forcing mechanism and attention matrix parts are wrong.
    Please check if the error is correct.

--- vignettes/examples/nmt_attention.R
+++ vignettes/examples/nmt_attention.R
@@ -287,7 +287,7 @@ evaluate <-
       c(preds, dec_hidden, attention_weights) %<-%
         decoder(list(dec_input, dec_hidden, enc_output))
       attention_weights <- k_reshape(attention_weights, c(-1))
-      attention_matrix[t,] <- attention_weights %>% as.double()
+      attention_matrix[t+1,] <- attention_weights %>% as.double()
       
       pred_idx <-
         tf$compat$v1$multinomial(k_exp(preds), num_samples = 1L)[1, 1] %>% as.double()
@@ -371,9 +371,9 @@ for (epoch in seq_len(n_epochs)) {
       for (t in seq_len(target_maxlen - 1)) {
         c(preds, dec_hidden, weights) %<-%
           decoder(list(dec_input, dec_hidden, enc_output))
-        loss <- loss + cx_loss(y[, t], preds)
+        loss <- loss + cx_loss(y[, t+1], preds)
         
-        dec_input <- k_expand_dims(y[, t])
+        dec_input <- k_expand_dims(y[, t+1])
       }
     })
     total_loss <-
